bbeff7a8ff4f29010a8cea7c782906855b58a417
The-LAW <joshuagboyega23@gmail.com>
2026-06-02 11:17:17 +0100

    made changes

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 const inputBox = document.getElementById("input-box");
-const taskRow = document.getElementById("taskrow");
+const listContainer = document.getElementById("list-container");
 
-function addTask(){
+function addTask() {
+    console.log("Button clicked")
     if(inputBox.value === ''){
         alert("You must write something");
+        return;
     }
+
+let taskRow = document.createElement("div");
+taskRow.className = "task-row";
+
+taskRow.innerHTMl = `
+<input type="checkbox">
+<p class="task-name">${inputBox.value}</p>
+<button class="button personal">Personal</button>
+
+<div class="task-date">
+<i class="fa-reguklar fa-calendar"></i>
+<span>June 10, 2026<?span>
+</div>
+
+<button class="btn medium">Medium</button>
+<i class="fa-solid fa-ellipsis"></i>
+`;
+
+listContainer.appendChild(taskRow);
+inputBox.value = "";
+saveData();
 }
-else{
-    let p = document.createElement("p")
-    p.innerHTML = inputBox.value;
-    taskRow.appendChild(p);
-let span = document.createElement("span");
-span.innerHTML = "\u00d7";
-p.appendChild(span);
+
+console.log(listContainer);
+listContainer.appendChild(newTask)
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === "INPUT" && e.target.type === "checkbox"){
+        e.target.parentElement.classList.toggle("completed");
+        saveData();
+    }
+    else if (e.target.tagName === "I" && e.target.classList.contains("fa-ellipsis")){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+function saveData(){
+    localStorage.setItem("tasks", listContainer.innerHTMl);
 }
+
